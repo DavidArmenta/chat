226a24e6239b9d0d974f3ca9503089a762923900
DavidArmenta <159225803+DavidArmenta@users.noreply.github.com>
2024-03-04 22:47:31 -0800

    agregamos wiget para dos mensajes

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,45 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_chat/widgets/her_message_bubble.dart';
+import 'package:yes_no_chat/widgets/my_message_bubble.dart';
 class ChatScreen extends StatelessWidget{
   const ChatScreen ({super.key});
  Widget build(BuildContext context){
-return Scaffold(
-  appBar: AppBar(
-   leading: const Padding(
-    padding:EdgeInsets.all(5),
-    child: CircleAvatar( 
-      backgroundImage: NetworkImage(
-        'https://i.pinimg.com/originals/85/dc/f8/85dcf8b9633c419f6794618d8d6bba52.jpg',
-        
-      ),
-    ),
-   ),
-title: const Text('nube voladora'),
-centerTitle: false,
-),
-// 
-body: const ChatView(),
+    return Scaffold(
+        appBar: AppBar(
+          leading: const Padding(
+             padding:EdgeInsets.all(5),
+                child: CircleAvatar( 
+                 backgroundImage: NetworkImage(
+                  'https://i.pinimg.com/originals/85/dc/f8/85dcf8b9633c419f6794618d8d6bba52.jpg',)),
+                    ),
+              title: const Text('nube voladora'),
+              centerTitle: false,
+                  ),
+                body: const _ChatView(),
 );
 }
 }
 
 
-class ChatView extends StatelessWidget {
-  const ChatView({super.key});
-  @override
-  
-  Widget build(BuildContext context) {
-    return SafeArea(child: Padding(padding: const EdgeInsets.symmetric(horizontal: 10),
-    child: Column(children: [
-      Expanded(child: Container(
-        color:  Colors.red,
-      )),
-    const Text('hola pa'),
-    const Text('que tal'),
+class _ChatView extends StatelessWidget {
+  const _ChatView({super.key});
 
-    ],
-    ),
-    )
-    )
-  ;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 101,
+                itemBuilder: ((context,index){
+                return (index % 2 == 0)
+                ? const HerMessageBubble()
+                : const MyMessageBubble();
+              })
+              ) 
+            ),
+            const Text("Texto 1"),
+            const Text("Texto 2")
+          ],
+        ),
+        )
+        );
   }
 }
