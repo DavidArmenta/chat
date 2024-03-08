7ee1e2303595278157251b42c6725cc2390e2e3e
DavidArmenta <159225803+DavidArmenta@users.noreply.github.com>
2024-03-07 22:37:43 -0800

    se hicieron cambios en el texbox para que se cierre al el teclado despues de enviar

--- lib/presentation/screens/chat/shared/message_field_box.dart
+++ lib/presentation/screens/chat/shared/message_field_box.dart
@@ -1,22 +1,86 @@
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-
+//typedef funcionOnPressed = 
 class MesageFieldBox extends StatelessWidget {
   const MesageFieldBox({super.key});
 
   @override
   Widget build(BuildContext context) {    
-    final colors= Theme.of(context).colorScheme;
-    return TextFormField(
-      decoration: const InputDecoration(
-        enabledBorder:OutlineInputBorder(
-          borderSide: BorderSide(color: colors.primary)),
+  Widget build(BuildContext context) {
+
+    final textController = TextEditingController();
+    final enfoque = FocusNode();
+
+
+    final UnderlineInputBorder outlineInputBorder = UnderlineInputBorder(
+      borderSide: const BorderSide(
+        color: Colors.transparent
+      ),
+      borderRadius: BorderRadius.circular(10),
+    );
+
+
+    final decoration = InputDecoration(
+      hintText: "Ingresa tu pregunta y termina con un -> ?",
+        enabledBorder: outlineInputBorder,
+        focusedBorder: outlineInputBorder,
         filled: true,
         suffixIcon: IconButton(
-         icon: const  Icons(Icons.send_and_archive_outlined),
-         onPressed: (){},
-        
-      ),
-    );
+          icon: const Icon(Icons.send_rounded),
+          onPressed: (){
+            final textValue = textController.value.text;
+            print("Impresion de Boton de Envio: $textValue");
+            textController.clear();
+          },)
+      );
+
+
+
+
+    return TextFormField(
+      controller: textController,
+      focusNode: enfoque,
+      onFieldSubmitted: (value){
+        print("Se ha mandado un valor: $value");
+        textController.clear();
+        enfoque.requestFocus();
+      },
+      onTapOutside:(event) {
+        enfoque.unfocus();
+      },
+      decoration: setDecoration(
+        inputBorder: OutlineInputBorder(), 
+        onPressed: ()=> onPress(
+          textController: textController
+          )));
+  
   }
+  UnderlineInputBorder OutlineInputBorder() => UnderlineInputBorder(
+      borderSide: const BorderSide(
+        color: Colors.transparent
+      ),
+      borderRadius: BorderRadius.circular(10),
+    );
+
+
+    InputDecoration setDecoration(
+      {required inputBorder, required void Function() onPressed })
+      =>InputDecoration(
+        enabledBorder: inputBorder,
+        focusedBorder: inputBorder,
+        filled: true,
+        suffixIcon: IconButton(
+          icon: const Icon(Icons.send_rounded),
+          onPressed: onPressed,
+        )
+      );
+
+      void onPress({required TextEditingController textController}){
+            final textValue = textController.value.text;
+            print("Impresion de Boton de Envio: $textValue");
+            textController.clear();
+      }
 }
